e510a84c8a6673011959360483a5cd7f92fd1c4d
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-07-01 20:06:27 -0300

    Added app config

---
+++ src/configs/app.rs
@@ -0,0 +1,18 @@
+use std::{env, sync::OnceLock};
+
+pub struct AppConfig {
+    pub port: u16,
+}
+
+static APP_CONFIG: OnceLock<AppConfig> = OnceLock::new();
+
+pub fn get_app_config() -> &'static AppConfig {
+    APP_CONFIG.get_or_init(|| {
+        let port = env::var("APP_PORT")
+            .expect("APP_PORT not found at .env file")
+            .parse()
+            .expect("APP_PORT needs be a number");
+
+        AppConfig { port }
+    })
+}
---
+++ src/configs/mod.rs
@@ -0,0 +1,2 @@
+mod app;
+pub use app::get_app_config;
