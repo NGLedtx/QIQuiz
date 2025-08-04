580160677b4f5fb50072969a9941d26bb00c6cec
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-08-04 19:57:13 -0300

    Removed migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,16 +1,10 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20250706_214212_create_table_rank;
-mod m20250706_221057_create_table_category;
-
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![
-            Box::new(m20250706_214212_create_table_rank::Migration),
-            Box::new(m20250706_221057_create_table_category::Migration),
-        ]
+        vec![]
     }
 }
