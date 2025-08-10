85926b19ebab5778b9f56558ab01e0a22a17b3e7
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-08-10 14:45:43 -0300

    Added route to delete quiz

---
+++ src/handlers/quiz/delete.rs
@@ -0,0 +1,119 @@
+use axum::{
+    body::Body,
+    extract::{Path, State},
+    http::{StatusCode, header::LOCATION},
+    response::Response,
+};
+use migration::{Expr, Query};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, TransactionTrait};
+
+use crate::{
+    entities::{alternative, question, quiz, rank},
+    jwt::JwtClaims,
+    state::AppState,
+};
+
+pub async fn delete(
+    State(state): State<AppState>,
+    token: JwtClaims,
+    Path(quiz_id): Path<i32>,
+) -> Response {
+    if !token.is_admin {
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/user/login")
+            .body(Body::empty())
+            .unwrap();
+    }
+
+    let db = &*state.db_conn;
+
+    let Ok(txn) = db.begin().await else {
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/quiz/list?error=true")
+            .body(Body::empty())
+            .unwrap();
+    };
+
+    if rank::Entity::delete_many()
+        .filter(rank::Column::IdQuiz.eq(quiz_id))
+        .exec(&txn)
+        .await
+        .is_err()
+    {
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/quiz/list?error=true")
+            .body(Body::empty())
+            .unwrap();
+    }
+
+    if alternative::Entity::delete_many()
+        .filter(Expr::exists(
+            Query::select()
+                .expr(1)
+                .from(question::Entity)
+                .and_where(
+                    Expr::col(question::Column::Id)
+                        .equals(alternative::Column::IdQuestion)
+                        .and(Expr::col(question::Column::IdQuiz).eq(quiz_id)),
+                )
+                .to_owned(),
+        ))
+        .exec(&txn)
+        .await
+        .is_err()
+    {
+        let _ = txn.rollback().await;
+
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/quiz/list?error=true")
+            .body(Body::empty())
+            .unwrap();
+    }
+
+    if question::Entity::delete_many()
+        .filter(question::Column::IdQuiz.eq(quiz_id))
+        .exec(&txn)
+        .await
+        .is_err()
+    {
+        let _ = txn.rollback().await;
+
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/quiz/list?error=true")
+            .body(Body::empty())
+            .unwrap();
+    }
+
+    if quiz::Entity::delete_by_id(quiz_id)
+        .exec(&txn)
+        .await
+        .is_err()
+    {
+        let _ = txn.rollback().await;
+
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/quiz/list?error=true")
+            .body(Body::empty())
+            .unwrap();
+    }
+
+    if txn.commit().await.is_err() {
+        return Response::builder()
+            .status(StatusCode::SEE_OTHER)
+            .header(LOCATION, "/quiz/list?error=true")
+            .body(Body::empty())
+            .unwrap();
+    };
+
+    Response::builder()
+        .status(StatusCode::SEE_OTHER)
+        .header(LOCATION, "/quiz/list?error=false")
+        .body(Body::empty())
+        .unwrap()
+}
--- src/routes/quiz.rs
+++ src/routes/quiz.rs
@@ -1,7 +1,12 @@
-use axum::{Router, routing::post};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
 use crate::{handlers, state::AppState};
 
 pub(super) fn configure_routes() -> Router<AppState> {
-    Router::new().route("/create", post(handlers::quiz::create))
+    Router::new()
+        .route("/create", post(handlers::quiz::create))
+        .route("/{quiz_id}/delete", get(handlers::quiz::delete))
 }
